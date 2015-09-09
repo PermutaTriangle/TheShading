c4d07bebd2f02338d7d4cee099d29f49b659f2eb
SuprDewd <suprdewd@gmail.com>
2015-09-09 02:41:44 +0000

    Refactor code

--- main.py
+++ main.py
@@ -4,186 +4,123 @@
 # 2. Create union-find of coincidences given by shading lemma (and possibly simultaneous shading lemma)
 # 3. Try to merge components that have same avoidance up to some length
 
+from permuta import (
+    Permutation,
+    Permutations,
+    MeshPattern,
+    MeshPatterns
+)
+from permuta.misc import UnionFind
 
-from permutation import Permutation
-from permutations import Permutations
-from mesh_pattern import MeshPattern
-from mesh_patterns import MeshPatterns
-from union_find import UnionFind
+# def subsets(elems):
+#     def bt(at, cur):
+#         if at == len(elems):
+#             yield cur
+#         else:
+#             for x in bt(at+1, cur): yield x
+#             for x in bt(at+1, cur + [elems[at]]): yield x
+#     for x in bt(0, []): yield x
+#
 
-def subsets(elems):
-    def bt(at, cur):
-        if at == len(elems):
-            yield cur
-        else:
-            for x in bt(at+1, cur): yield x
-            for x in bt(at+1, cur + [elems[at]]): yield x
-    for x in bt(0, []): yield x
-
-n = 3
+# n = 2
 # check_len = 7
-check_len = 0
-simultaneous = True
-closure = True
+#
+# ss = {}
+# for i in range(cnt):
+#     ss.setdefault(uf.find(i),[])
+#     ss[uf.find(i)].append(i)
+#
+# for _,v in sorted(ss.items(),key=lambda k: min(k[1])):
+#     # print('{%s}' % ','.join(map(str,sorted(v))))
+#
+#     if len(v) > 1:
+#         # Sanity check: make sure patterns in the same group are avoided by the same permutations
+#         ans = None
+#         for i in v:
+#             mp = mps[i]
+#             av = []
+#             for l in range(1,check_len+1):
+#                 for p in Permutations(l):
+#                     if p.avoids(mp):
+#                         av.append(p)
+#             if ans is None:
+#                 ans = av
+#             assert av == ans
+#
+# # DONE: closure (sjá hvað breytist)
+# # DONE: fall sem tekur við möskvamynstri og skilar flokknum sem það er í
+# # TODO: láta input vera umröðun
+# # DONE: aðskilja kóða, einfalt að gera shading lemma eða s. shading lemma +- closure
+#
 
 mps = []
-idx = {}
-for i,mp in enumerate(MeshPatterns(n)):
-    mps.append(mp)
-    idx[mp] = i
-    # print(i, mp)
+mpid = {}
+uf = UnionFind()
 
-cnt = len(mps)
-uf = UnionFind(cnt)
+def get_mpid(mp):
+    if mp not in mpid:
+        mpid[mp] = uf.add()
+        assert mpid[mp] == len(mps)
+        mps.append(mp)
+    return mpid[mp]
 
-bla = 0
-for mp in mps:
-    if bla % 1000 == 0:
-        print('A', bla)
-    bla += 1
+def get_class(mp, simultaneous=True, closure=True):
+    visited = set([mp])
+    stack = [mp]
+    while stack:
+        cur = stack.pop()
 
-    poss = []
-    for i in range(n+1):
-        for j in range(n+1):
-            ans = mp.can_shade((i,j))
-            if simultaneous and (i,j) not in mp.mesh and ans:
-                poss.append((ans, set([(i,j)])))
-            if not simultaneous and ans:
-                uf.unite(idx[mp], idx[mp.shade((i,j))])
+        poss = []
+        for i in range(len(cur)+1):
+            for j in range(len(cur)+1):
+                # TODO: only go through boxes that have an adjacent point
+                ans = cur.can_shade((i,j))
+                if ans:
+                    if simultaneous:
+                        if (i,j) not in cur.mesh:
+                            poss.append((ans, set([(i,j)])))
+                    else:
+                        nxt = cur.shade((i,j))
+                        if nxt not in visited:
+                            stack.append(nxt)
+                            visited.add(nxt)
 
-    if simultaneous:
-        for i in range(n+1):
-            for j in range(n+1):
-                for di in range(-1,2):
-                    for dj in range(-1,2):
-                        if (di == 0) == (dj == 0):
-                            continue
-                        i2,j2 = (i+di, j+dj)
-                        if 0 <= i2 < n+1 and 0 <= j2 < n+1:
-                            ans = mp.can_shade2((i,j),(i2,j2))
-                            if ans:
-                                poss.append((ans, set([(i,j),(i2,j2)])))
+        if simultaneous:
+            for i in range(len(cur)+1):
+                for j in range(len(cur)+1):
+                    for di in range(-1,2):
+                        for dj in range(-1,2):
+                            if (di == 0) == (dj == 0):
+                                continue
+                            # TODO: only go through pairs of boxes that have an adjacent point
+                            i2,j2 = (i+di, j+dj)
+                            if 0 <= i2 < len(cur)+1 and 0 <= j2 < len(cur)+1:
+                                ans = cur.can_shade2((i,j),(i2,j2))
+                                if ans:
+                                    poss.append((ans, set([(i,j),(i2,j2)])))
 
-        def bt(at, nm, used):
-            if at == len(poss):
-                mp2 = mp.shade(nm)
-                uf.unite(idx[mp], idx[mp2])
-                return
+            def bt(at, nm, used):
+                if at == len(poss):
+                    nxt = cur.shade(nm)
+                    if nxt not in visited:
+                        stack.append(nxt)
+                        visited.add(nxt)
+                    return
 
-            bt(at + 1, nm, used)
-            if poss[at][0] not in used and not (nm & poss[at][1]):
-                bt(at + 1, nm | poss[at][1], used | set([poss[at][0]]))
+                bt(at + 1, nm, used)
+                if poss[at][0] not in used and not (nm & poss[at][1]):
+                    bt(at + 1, nm | poss[at][1], used | set([poss[at][0]]))
 
-        bt(0, set(), set())
+            bt(0, set(), set())
+    return visited
 
-# print(len(found))
+mp = MeshPatterns(5).random_element()
 
-if closure:
-    ss = {}
-    for i in range(cnt):
-        ss.setdefault(uf.find(i),[])
-        ss[uf.find(i)].append(i)
-
-    bla = 0
-    for _,v in sorted(ss.items(),key=lambda k: min(k[1])):
-        if bla % 1000 == 0:
-            print('B', bla)
-        bla += 1
-
-        minima = []
-        maxima = []
-        for i in v:
-            mp_cur = mps[i]
-            is_maximal = True
-            is_minimal = True
-            for j in v:
-                if i == j:
-                    continue
-                mp = mps[j]
-                if mp_cur.mesh <= mp.mesh:
-                    is_maximal = False
-                if mp.mesh <= mp_cur.mesh:
-                    is_minimal = False
-            if is_maximal:
-                maxima.append(mp_cur)
-            if is_minimal:
-                minima.append(mp_cur)
-
-        for mn in minima:
-            for mx in maxima:
-                if mn <= mx:
-                    for add in subsets(list(mx.mesh - mn.mesh)):
-                        mid = mn.shade(set(add))
-                        uf.unite(idx[mn], idx[mid])
-
-ss = {}
-for i in range(cnt):
-    ss.setdefault(uf.find(i),[])
-    ss[uf.find(i)].append(i)
-
-res = 0
-for _,v in sorted(ss.items(),key=lambda k: min(k[1])):
-    res += 1
-    # print('{%s}' % ','.join(map(str,sorted(v))))
-
-    if len(v) > 1:
-        # Sanity check: make sure patterns in the same group are avoided by the same permutations
-        ans = None
-        for i in v:
-            mp = mps[i]
-            av = []
-            for l in range(1,check_len+1):
-                for p in Permutations(l):
-                    if p.avoids(mp):
-                        av.append(p)
-            if ans is None:
-                ans = av
-            assert av == ans
-
-print(res)
-
-# def _rot_right(n,pos):
-#     x,y = pos
-#     assert 0 <= x < n+1
-#     assert 0 <= y < n+1
-#     return (y,n-x)
-#
-# # pos1 = (2,2)
-# # pos2 = (2,1)
-# # mp = MeshPattern(Permutation([2,1]), [])
-# # for i in range(2):
-# #     mp = mp.rotate_right()
-# #     pos1 = _rot_right(2, pos1)
-# #     pos2 = _rot_right(2, pos2)
-# # print(mp)
-# # print(pos1, pos2)
-# # print mp._can_shade2(pos1, pos2)
-#
-# for mp in mps:
-#     for i in range(n+1):
-#         for j in range(n+1):
-#             for di in range(-1,2):
-#                 for dj in range(-1,2):
-#                     if (di == 0) == (dj == 0):
-#                         continue
-#                     i2,j2 = (i+di, j+dj)
-#                     if 0 <= i2 < n+1 and 0 <= j2 < n+1:
-#                         print(mp, i,j, i2, j2)
-#                         print mp.can_shade((i,j))
-#                         print mp.can_shade((i2,j2))
-#                         print mp.can_shade2((i,j),(i2,j2))
-#                         if mp.can_shade2((i,j),(i2,j2)):
-#                             assert mp.can_shade((i,j)) and mp.can_shade((i2,j2))
-#
-#                         # assert (mp.can_shade((i,j)) and mp.can_shade((i2,j2))) == mp.can_shade2((i,j),(i2,j2))
-
-
-# mp = MeshPattern(Permutation([1,3,2]), [(0,1)])
-# print mp.can_shade2((1,3),(2,3))
-
-
-# TODO: closure (sjá hvað breytist)
-# TODO: fall sem tekur við möskvamynstri og skilar flokknum sem það er í
-# TODO: láta input vera umröðun
-# TODO: aðskilja kóða, einfalt að gera shading lemma eða s. shading lemma +- closure
+cnt = 0
+for v in get_class(mp, True, True):
+    print(repr(v))
+    print(v)
+    print('')
+    cnt += 1
+print(cnt)
 
