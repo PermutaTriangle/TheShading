5f50e0979ab466beee9f56b8a65d5f5686d0fde4
SuprDewd <suprdewd@gmail.com>
2015-10-30 12:31:25 +0000

    Added more results

---
+++ the_shading_algorithm/surprising_coincidences/tsa2_simple/C004_123_1/__init__.py
@@ -0,0 +1,3 @@
+from permuta import *
+mp1 = MeshPattern(Permutation([1, 2, 3]), set([(1, 2), (1, 3), (3, 0), (3, 1), (2, 0), (2, 3), (2, 2)]))
+mp2 = MeshPattern(Permutation([1, 2, 3]), set([(3, 2), (1, 3), (3, 0), (3, 1), (2, 1), (2, 0), (2, 2)]))
---
+++ the_shading_algorithm/surprising_coincidences/tsa2_simple/C006_123_1/__init__.py
@@ -0,0 +1,3 @@
+from permuta import *
+mp1 = MeshPattern(Permutation([1, 2, 3]), set([(0, 1), (1, 2), (3, 0), (3, 1), (1, 1), (2, 0), (0, 2)]))
+mp2 = MeshPattern(Permutation([1, 2, 3]), set([(3, 0), (3, 1), (2, 1), (1, 1), (2, 0), (1, 0), (0, 2)]))
