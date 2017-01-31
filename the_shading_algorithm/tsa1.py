cb05f3fa091b32bc5bc7c32c397be32da67df3df
Tomas Ken <tomkenmag@gmail.com>
2017-01-31 11:24:54 +0000

    Updated tsa1 to work with new permuta, should be working?

--- the_shading_algorithm/tsa1.py
+++ the_shading_algorithm/tsa1.py
@@ -23,9 +23,19 @@ for i, v in enumerate(MSGS):
     locals()[v[0]] = i
 
 def tsa1(p, B, force, maxdepth, cut=False):
-    assert B not in p.mesh
+    """
+    Args:
+        p: <permuta.MeshPatt>
+            The mesh pattern.
+        B: tuple(<numbers.Integral, numbers.Integral>)
+            Point to insert into.
+        force: tuple(<numbers.Integral, numbers.Integral>)
+            Index and force direction.
+
+    """
+    assert B not in p.shading
     q = p.shade(B)
-    k = len(p.perm)
+    k = len(p)
 
     global instr
     instr = []
@@ -47,7 +57,7 @@ def tsa1(p, B, force, maxdepth, cut=False):
         return Popper(len(ms))
 
     instr.append((INITIAL_PATTERN, p))
-    instr.append((CONSIDER_OCCURRENCE, p.perm.perm[force[0]], STR_ADJ[force[1]]))
+    instr.append((CONSIDER_OCCURRENCE, p.pattern[force[0]], STR_ADJ[force[1]]))
 
     traces = []
     def dfs(imp, putin, impxval, impyval, seen, depth_cutoff):
@@ -87,7 +97,8 @@ def tsa1(p, B, force, maxdepth, cut=False):
                 ]):
 
                 for occ in choose(len(nxtxval), k):
-                    hereperm = [ nxt.perm[occ[i]] for i in range(k) ]
+                    print(occ)
+                    hereperm = [ nxt.pattern[occ[i]] for i in range(k) ]
                     herexval = [ nxtxval[occ[i]] for i in range(k) ]
                     hereyval = [ nxtyval[hereperm[i]-1] for i in range(k) ]
 
@@ -95,14 +106,14 @@ def tsa1(p, B, force, maxdepth, cut=False):
                         continue
                     nseen = set(list(seen))
                     nseen.add(tuple(herexval))
-                    if not (Permutation.to_standard(hereperm) == p.perm):
+                    if not (Perm.to_standard(hereperm) == p.pattern):
                         continue
 
                     #print occ
                     # print hereperm
 
-                    sub = nxt.sub_mesh([ x+1 for x in occ ])
-                    add = p.mesh - sub.mesh
+                    sub = nxt.sub_mesh_pattern([ x for x in occ ])
+                    add = p.shading - sub.shading
                     # print add
 
                     # print sub
@@ -110,21 +121,23 @@ def tsa1(p, B, force, maxdepth, cut=False):
                     #print hereyval
                     #print add
 
+                    print(p)
+
                     if len(add) == 0:
                         if force[1] == 0 and herexval[force[0]] > force[0]+1:
-                            with msg([(CONSIDER_SUB_RIGHT, ', '.join([str(i) for i in occ]), sub.perm.perm[force[0]])]):
+                            with msg([(CONSIDER_SUB_RIGHT, ', '.join([str(i) for i in occ]), sub.pattern[force[0]])]):
                                 traces.append(list(instr))
                                 return True
-                        elif force[1] == 1 and hereyval[p.perm[force[0]]-1] > p.perm[force[0]]:
-                            with msg([(CONSIDER_SUB_UP, ', '.join( [str(i) for i in occ]), sub.perm.perm[force[0]])]):
+                        elif force[1] == 1 and hereyval[p.pattern[force[0]]-1] > p.pattern[force[0]]:
+                            with msg([(CONSIDER_SUB_UP, ', '.join( [str(i) for i in occ]), sub.pattern[force[0]])]):
                                 traces.append(list(instr))
                                 return True
                         elif force[1] == 2 and herexval[force[0]] < force[0]+1:
-                            with msg([(CONSIDER_SUB_LEFT, ', '.join( [str(i) for i in occ]), sub.perm.perm[force[0]])]):
+                            with msg([(CONSIDER_SUB_LEFT, ', '.join( [str(i) for i in occ]), sub.pattern[force[0]])]):
                                 traces.append(list(instr))
                                 return True
-                        elif force[1] == 3 and hereyval[p.perm[force[0]]-1] < p.perm[force[0]]:
-                            with msg([(CONSIDER_SUB_RIGHT, ', '.join( [str(i) for i in occ]), sub.perm.perm[force[0]])]):
+                        elif force[1] == 3 and hereyval[p.pattern[force[0]]-1] < p.pattern[force[0]]:
+                            with msg([(CONSIDER_SUB_RIGHT, ', '.join( [str(i) for i in occ]), sub.pattern[force[0]])]):
                                 traces.append(list(instr))
                                 return True
 
@@ -140,11 +153,11 @@ def tsa1(p, B, force, maxdepth, cut=False):
 
                     if force[1] == 0 and herexval[force[0]] <= force[0]+1:
                         continue
-                    elif force[1] == 1 and hereyval[p.perm[force[0]]-1] <= p.perm[force[0]]:
+                    elif force[1] == 1 and hereyval[p.pattern[force[0]]-1] <= p.pattern[force[0]]:
                         continue
                     elif force[1] == 2 and herexval[force[0]] >= force[0]+1:
                         continue
-                    elif force[1] == 3 and hereyval[p.perm[force[0]]-1] >= p.perm[force[0]]:
+                    elif force[1] == 3 and hereyval[p.pattern[force[0]]-1] >= p.pattern[force[0]]:
                         continue
 
                     txval = [0] + herexval + [k+1]
@@ -158,11 +171,11 @@ def tsa1(p, B, force, maxdepth, cut=False):
                     putin2 = set()
                     for x in range(left,right):
                         for y in range(down,up):
-                            if (x,y) not in nxt.mesh:
+                            if (x,y) not in nxt.shading:
                                 putin2.add((x,y))
                     inside = False
                     for x in range(left, right-1):
-                        if down < nxt.perm[x] < up:
+                        if down < nxt[x] < up:
                             inside = True
                             break
                     if inside:
@@ -176,19 +189,25 @@ def tsa1(p, B, force, maxdepth, cut=False):
                     with msg([
                             (CONSIDER_SUB, ', '.join([str(i) for i in occ])),
                             (WHICH_PATTERN, sub),
-                            (ANOTHER_OCC, add[0], add[1], left, down, right, up, sub.perm.perm[force[0]], STR_ADJ2[force[1]]),
+                            (ANOTHER_OCC, add[0], add[1], left, down, right, up, sub.pattern[force[0]], STR_ADJ2[force[1]]),
                         ]):
 
                         if dfs(nxt, pi, nxtxval, nxtyval, nseen, depth_cutoff-1) and cut:
                             return True
         return False
 
-    dfs(p, B, [ i+1 for i in range(k) ], [ i+1 for i in range(k) ], set([tuple(p.perm.perm)]), maxdepth)
+    dfs(p, B, [ i+1 for i in range(k) ], [ i+1 for i in range(k) ], set([tuple(p.pattern)]), maxdepth)
     return [ [ MSGS[t[0]][1].format(*t[1:]) for t in ts ] for ts in traces ]
 
 def all_points_all_dir(mp, B, maxdepth, cut=False):
+    """
+    Args:
+        mp: <permuta.MeshPatt>
+        B: tuple(<numbers.Integral>, <numbers.Integral>)
+        maxdepth: <numbers.Integral>
+    """
     all_traces = []
-    for i in range(len(mp.perm.perm)):
+    for i in range(len(mp.pattern)):
         for d in range(4):
             all_traces += tsa1(mp, B, (i,d), maxdepth, cut=cut)
             if cut and all_traces: return all_traces
@@ -196,28 +215,28 @@ def all_points_all_dir(mp, B, maxdepth, cut=False):
 
 if __name__ == '__main__':
 
-    # run = all_points_all_dir(MeshPattern(Permutation([1]), [(1,1)]), (0,1), 5)
+    # run = all_points_all_dir(MeshPatt(Perm([1]), [(1,1)]), (0,1), 5)
 
     #C1
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), 3)
-    #run = tsa1(MeshPattern(Permutation([1,2,3]), [(0,0),(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), (1, 3),  3)
+    run = all_points_all_dir(MeshPatt(Perm([0,1,2]), [(0,0),(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), 3)
+    # run = tsa1(MeshPatt(Perm([1,2,3]), [(0,0),(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), (1, 3),  3)
 
     #C2
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), 4)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), 4)
 
     #C5
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1), 3)
 
     # ---------------------------------------------------------------------------- #
 
     #C8
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,3),(1,0),(1,1),(1,2),(2,0),(2,2),(3,0)]), (2,3), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,3),(1,0),(1,1),(1,2),(2,0),(2,2),(3,0)]), (2,3), 3)
 
     #C9
-    #run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(0,3),(1,0),(1,1),(1,2),(2,0),(2,2),(3,0)]), (2,3), 5)
+    #run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(0,3),(1,0),(1,1),(1,2),(2,0),(2,2),(3,0)]), (2,3), 5)
 
     #C14
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,0),(1,1),(2,3),(3,0),(3,1),(3,2),(3,2),(3,3)]), (2,1), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,0),(1,1),(2,3),(3,0),(3,1),(3,2),(3,2),(3,3)]), (2,1), 3)
 
     # C15
     # C16
@@ -225,102 +244,102 @@ if __name__ == '__main__':
     # ---------------------------------------------------------------------------- #
 
     # C17
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,1),(1,3),(2,1),(2,2),(2,3),(3,0),(3,3)]), (1,0), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,1),(1,3),(2,1),(2,2),(2,3),(3,0),(3,3)]), (1,0), 3)
 
     # C18
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,1),(1,3),(2,1),(2,2),(2,3),(3,0)]), (1,0), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,1),(1,3),(2,1),(2,2),(2,3),(3,0)]), (1,0), 3)
 
     # ---------------------------------------------------------------------------- #
 
     # C19
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,0),(1,3),(2,1),(3,0)]), (3,3), 10)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,0),(1,3),(2,1),(3,0)]), (3,3), 10)
 
     # C20
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,3),(2,1),(3,0)]), (3,3), 5)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,3),(2,1),(3,0)]), (3,3), 5)
 
     # ---------------------------------------------------------------------------- #
 
     # C21
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(0,1),(1,1),(2,1),(2,2),(3,0)]), (3,2), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(0,1),(1,1),(2,1),(2,2),(3,0)]), (3,2), 3)
 
     # C22
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,0),(1,1),(2,1),(2,2),(3,0)]), (3,2), 6)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,0),(1,1),(2,1),(2,2),(3,0)]), (3,2), 6)
 
     # C23
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,1),(2,1),(2,2),(3,0)]), (3,2), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,1),(2,1),(2,2),(3,0)]), (3,2), 3)
 
     # ---------------------------------------------------------------------------- #
 
     # C24
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,1),(2,1),(2,2),(2,3),(3,0),(3,3)]), (1,0), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,1),(2,1),(2,2),(2,3),(3,0),(3,3)]), (1,0), 3)
 
     # ---------------------------------------------------------------------------- #
 
     # C25
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,0),(1,1),(1,2),(2,3),(3,0)]), (3,3), 9)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,0),(1,1),(1,2),(2,3),(3,0)]), (3,3), 9)
 
     # C26
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,1),(1,2),(2,3),(3,0)]), (3,3), 9)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,1),(1,2),(2,3),(3,0)]), (3,3), 9)
 
     # C27
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,2),(2,3),(3,0)]), (3,3), 9)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,2),(2,3),(3,0)]), (3,3), 9)
 
     # C28
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,2),(2,3),(3,0)]), (3,3), 9)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,2),(2,3),(3,0)]), (3,3), 9)
 
     # C29
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,1),(1,2),(2,3),(3,0)]), (3,3), 9)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,1),(1,2),(2,3),(3,0)]), (3,3), 9)
 
     # ---------------------------------------------------------------------------- #
 
     # C30 This is a chain
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,0),(1,1),(2,3),(3,0)]), (2,1), 3)
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,0),(1,0),(1,1),(2,1),(2,3),(3,0)]), (3,3), 4)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,0),(1,1),(2,3),(3,0)]), (2,1), 3)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,0),(1,0),(1,1),(2,1),(2,3),(3,0)]), (3,3), 4)
 
     # ---------------------------------------------------------------------------- #
 
     # C31
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(1,1),(2,1),(2,2),(2,3),(3,0)]), (1,0), 4)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(1,1),(2,1),(2,2),(2,3),(3,0)]), (1,0), 4)
 
     # ---------------------------------------------------------------------------- #
 
     # C32
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(1,2),(2,2),(3,0),(3,3)]), (0,0), 8)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(1,2),(2,2),(3,0),(3,3)]), (0,0), 8)
 
     # C33
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(1,2),(2,2),(3,0),(3,2),(3,3)]), (0,0), 8)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(1,2),(2,2),(3,0),(3,2),(3,3)]), (0,0), 8)
 
     # C34
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(1,2),(3,0),(3,3)]), (0,0), 10)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(1,2),(3,0),(3,3)]), (0,0), 10)
 
     # C35
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(1,2),(2,2),(3,0)]), (0,0), 10)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(1,2),(2,2),(3,0)]), (0,0), 10)
 
     # C36
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(1,2),(3,0)]), (0,0), 10)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(1,2),(3,0)]), (0,0), 10)
 
     # ---------------------------------------------------------------------------- #
 
     # ---------------------------------------------------------------------------- #
 
     # C37
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(1,1),(2,1),(2,2),(3,0)]), (3,2), 6)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(1,1),(2,1),(2,2),(3,0)]), (3,2), 6)
 
     # ---------------------------------------------------------------------------- #
 
     # C38 This is a chain
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(2,2),(3,0),(3,2),(3,3)]), (2,1), 6)
-    # run = all_points_all_dir(MeshPattern(Permutation([1,2,3]), [(0,1),(2,1),(2,2),(3,0),(3,2),(3,3)]), (0,0), 6)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(2,2),(3,0),(3,2),(3,3)]), (2,1), 6)
+    # run = all_points_all_dir(MeshPatt(Perm([1,2,3]), [(0,1),(2,1),(2,2),(3,0),(3,2),(3,3)]), (0,0), 6)
 
     # ---------------------------------------------------------------------------- #
 
     # ---------------------------------------------------------------------------- #
 
     # run = tsa1(meshpattern(permutation([1,2,3]), [(0,0),(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1))
-    # run = tsa1(MeshPattern(Permutation([1,2,3]), [(0,0),(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (1,1))
-    # run = tsa1(MeshPattern(Permutation([1,2,3]), [(1,0),(1,3),(2,1),(3,0)]), (3,3))
-    # run = tsa1(MeshPattern(Permutation([1,2,3]), [(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1))
-    # run = tsa1(MeshPattern(Permutation([1,2,3]), [(0,0),(1,0),(1,1),(2,3),(3,0),(3,1)]), (2,1))
+    # run = tsa1(MeshPatt(Perm([1,2,3]), [(0,0),(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (1,1))
+    # run = tsa1(MeshPatt(Perm([1,2,3]), [(1,0),(1,3),(2,1),(3,0)]), (3,3))
+    # run = tsa1(MeshPatt(Perm([1,2,3]), [(0,1),(1,0),(2,0),(2,2),(3,0),(3,2),(3,3)]), (2,1))
+    # run = tsa1(MeshPatt(Perm([1,2,3]), [(0,0),(1,0),(1,1),(2,3),(3,0),(3,1)]), (2,1))
 
 print("\n================================================================================\n".join(['\n'.join(i) for i in run]))
 print("\nTotal number of successful branches: {}\n".format(len(run)))
