bc5a34656abe0664e16898679870b47d0f65d7d2
Tomas Ken <tomkenmag@gmail.com>
2017-03-08 23:39:12 +0000

    jobs to run...

--- the_shading_algorithm/surpr_coincidences/run.sh
+++ the_shading_algorithm/surpr_coincidences/run.sh
@@ -1,6 +1,14 @@
-python classify.py ../surprising_coincidence_classification_012_permlen9.txt -tsa1 1 > surprising_coincidence_012_TS1_depth1.txt
-python classify.py ../surprising_coincidence_classification_021_permlen9.txt -tsa1 1 > surprising_coincidence_021_TS1_depth1.txt
 
-python classify.py surprising_coincidence_classification_01_permlen5.txt -ssl > surprising_coincidence_01_SSL.txt
-python classify.py surprising_coincidence_classification_012_permlen9.txt -ssl > surprising_coincidence_012_SSL.txt
-python classify.py surprising_coincidence_classification_021_permlen9.txt -ssl > surprising_coincidence_021_SSL.txt
+#printf "(1,2) Simultaneous Shading lemma\n"
+#python classify.py surprising_coincidence_classification_01_permlen5.txt -ssl > surprising_coincidence_01_SSL.txt
+#printf "(1,2,3) Simultaneous Shading lemma\n"
+#python classify.py surprising_coincidence_classification_012_permlen9.txt -ssl > surprising_coincidence_012_SSL.txt
+#printf "(1,3,2) Simultaneous Shading lemma\n"
+#python classify.py surprising_coincidence_classification_021_permlen9.txt -ssl > surprising_coincidence_021_SSL.txt
+
+#printf "(1,2,3) TSA1 - depth 1\n"
+#python classify.py surprising_coincidence_classification_012_permlen9.txt -tsa1 1 > surprising_coincidence_012_TSA1_depth1.txt
+printf "(1,2) TSA1 - depth 1\n"
+python classify.py surprising_coincidence_classification_01_permlen5.txt -tsa1 1 > surprising_coincidence_01_TSA1_depth1.txt
+printf "(1,3,2) TSA1 - depth 1\n"
+python classify.py surprising_coincidence_classification_021_permlen9.txt -tsa1 1 > surprising_coincidence_021_TSA1_depth1.txt
