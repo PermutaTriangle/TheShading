2c86de5ac43e779093825896b6614792759be0bd
Tomas <tomkenmag@gmail.com>
2017-03-10 10:54:33 +0000

    results

--- the_shading_algorithm/surpr_coincidences/run.sh
+++ the_shading_algorithm/surpr_coincidences/run.sh
@@ -12,9 +12,16 @@ python classify.py surprising_coincidence_classification_021_permlen9.txt -sl > 
 #printf "(1,3,2) Simultaneous Shading lemma\n"
 #python classify.py surprising_coincidence_classification_021_permlen9.txt -ssl > surprising_coincidence_021_SSL.txt
 
-# printf "(1,2) TSA1 - depth 1\n"
-# python classify.py surprising_coincidence_01_SL.txt -tsa1 1 > surprising_coincidence_01_SL_TSA1_depth1.txt
-# printf "(1,2,3) TSA1 - depth 1\n"
-# python classify.py surprising_coincidence_012_SL.txt -tsa1 1 > surprising_coincidence_012_SL_TSA1_depth1.txt
-# printf "(1,3,2) TSA1 - depth 1\n"
-# python classify.py surprising_coincidence_021_SL.txt -tsa1 1 > surprising_coincidence_021_SL_TSA1_depth1.txt
+printf "(1,2) Lemma 2\n"
+python classify.py surprising_coincidence_01_SL.txt -lemma2 > surprising_coincidence_01_SL_Lemma2.txt
+printf "(1,2,3) Lemma 2\n"
+python classify.py surprising_coincidence_012_SL.txt -lemma2 > surprising_coincidence_012_SL_Lemma2.txt
+printf "(1,3,2) Lemma 2\n"
+python classify.py surprising_coincidence_021_SL.txt -lemma2 > surprising_coincidence_021_SL_Lemma2.txt
+
+printf "(1,2) Lemma 5\n"
+python classify.py surprising_coincidence_01_SL.txt -lemma5 > surprising_coincidence_01_SL_Lemma5.txt
+printf "(1,2,3) Lemma 5\n"
+python classify.py surprising_coincidence_012_SL.txt -lemma5 > surprising_coincidence_012_SL_Lemma5.txt
+printf "(1,3,2) Lemma 5\n"
+python classify.py surprising_coincidence_021_SL.txt -lemma5 > surprising_coincidence_021_SL_Lemma5.txt
