803e6d87c6766268b90bcf5bb2f096d8c7cacfec
Tomas Ken <tomkenmag@gmail.com>
2017-03-16 14:21:50 +0000

    aoeu

---
+++ the_shading_algorithm/surpr_coincidences/run_lem7.sh
@@ -0,0 +1,26 @@
+
+
+#function dopatt {
+    #for i in {0..32}; do
+        #python classify.py results/surprising_coincidence_01_SL_Lemma7_$i.txt -lemma7 > results/surprising_coincidence_01_SL_Lemma7_$((i + 1)).txt
+
+        #diff results/surprising_coincidence_01_SL_Lemma7_$i.txt results/surprising_coincidence_01_SL_Lemma7_$((i + 1)).txt 2> /dev/null
+
+        #if [ $? -eq 0 ]; then
+            #break
+        #fi
+    #done
+#}
+
+for p in {"01","012","021"}; do
+    for i in {0..32}; do
+        python classify.py results/surprising_coincidence_"$p"_SL_Lemma7_"$i".txt -lemma7 > results/surprising_coincidence_"$p"_SL_Lemma7_$((i + 1)).txt
+
+        diff results/surprising_coincidence_"$p"_SL_Lemma7_"$i".txt results/surprising_coincidence_"$p"_SL_Lemma7_$((i + 1)).txt 2> /dev/null
+
+        if [ $? -eq 0 ]; then
+            break
+        fi
+    done
+done
+
