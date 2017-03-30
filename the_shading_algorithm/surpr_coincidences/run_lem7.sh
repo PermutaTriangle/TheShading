bac6da549a4519d0dea85e2126eb558b44ff6b14
Tomas <tomkenmag@gmail.com>
2017-03-30 14:54:32 +0000

    running lem7 to more depths

--- the_shading_algorithm/surpr_coincidences/run_lem7.sh
+++ the_shading_algorithm/surpr_coincidences/run_lem7.sh
@@ -13,14 +13,19 @@
 #}
 
 for p in {"01","012","021"}; do
-    for i in {0..32}; do
-        python classify.py results/lemma7/surprising_coincidence_"$p"_SL_Lemma7_"$i".txt -lemma7 > results/lemma7/surprising_coincidence_"$p"_SL_Lemma7_$((i + 1)).txt
+    for d in {2..7}; do
+        cp results/lemma7/depth_$((d - 1))/surprising_coincidence_"$p"_SL_Lemma7_final.txt results/lemma7/depth_"$d"/surprising_coincidence_"$p"_SL_Lemma7_0.txt
 
-        diff results/lemma7/surprising_coincidence_"$p"_SL_Lemma7_"$i".txt results/lemma7/surprising_coincidence_"$p"_SL_Lemma7_$((i + 1)).txt 2> /dev/null
+        for i in {0..32}; do
+            python classify.py results/lemma7/depth_"$d"/surprising_coincidence_"$p"_SL_Lemma7_"$i".txt -lemma7 > results/lemma7/depth_"$d"/surprising_coincidence_"$p"_SL_Lemma7_$((i + 1)).txt
 
-        if [ $? -eq 0 ]; then
-            break
-        fi
+            diff results/lemma7/depth_"$d"/surprising_coincidence_"$p"_SL_Lemma7_"$i".txt results/lemma7/depth_"$d"/surprising_coincidence_"$p"_SL_Lemma7_$((i + 1)).txt 2> /dev/null
+
+            if [ $? -eq 0 ]; then
+                cp results/lemma7/depth_"$d"/surprising_coincidence_"$p"_SL_Lemma7_$((i + 1)).txt results/lemma7/depth_"$d"/surprising_coincidence_"$p"_SL_Lemma7_final.txt
+                break
+            fi
+        done
     done
 done
 
