5edb62d8c3313a8e90b79be042704810c722fd3e
Tomas Ken <tomkenmag@gmail.com>
2017-05-03 16:16:28 +0000

    for garpur

--- the_shading_algorithm/surpr_coincidences/sanity_checker.py
+++ the_shading_algorithm/surpr_coincidences/sanity_checker.py
@@ -1,5 +1,5 @@
 
-import sys
+import os, sys
 from permuta import *
 from permuta.misc import ProgressBar, factorial
 from classify import parse_classes, ExpClasses
@@ -7,17 +7,18 @@ from tsa5_knowledge import tsa5_two as tsa5
 
 maxlength = int(sys.argv[1])
 cpatt = Perm(map(int, sys.argv[2].split()))
-mpatts = [MeshPatt.unrank(cpatt, m) for m in map(int, sys.argv[3:])]
+# mpatts = [MeshPatt.unrank(cpatt, m) for m in map(int, sys.argv[3:])]
+JOBID = int(os.environ['SLURM_ARRAY_TASK_ID'])
+mpatts = [MeshPatt.unrank(cpatt, mpatt) for mpatt in list(map(int, open(sys.argv[3]).readlines()[JOBID].split()))]
+sys.stderr.write(str([mpatt.rank() for mpatt in mpatts]))
+sys.stderr.write("\n")
 
 for l in range(len(cpatt), maxlength + 1):
     sys.stderr.write("Length {} permutations\n".format(l))
-    # ProgressBar.create(factorial(l))
     for p in PermSet(l):
-        # ProgressBar.progress()
         if len(set([p.avoids(mpatt) for mpatt in mpatts])) != 1:
             print("Found permutation")
             print(cpatt)
             print(mpatts)
             print([p.avoids(mpatt) for mpatt in mpatts])
             sys.exit(0)
-    # ProgressBar.finish()
